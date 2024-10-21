8dd911d917b4c92e2a2950b646f1eee242a0e7c3
Janek <jn.smialek@gmail.com>
2024-10-21 17:01:03 +0100

    added html and flask basic

--- pokemon_back_end_api.py
+++ pokemon_back_end_api.py
@@ -1,4 +1,8 @@
 import requests
+import flask
+from flask import jsonify
+
+MAX_POKEMON = 50
 
 #
 # The API should respond with the following information:
@@ -6,16 +10,35 @@ import requests
 # - The silhouette image of the Pokémon.
 # - A list of four Pokémon names (the correct name and three decoy names)
 #
+def getTrueName(pokemonID):
+    # TODO REPLACE WITH API CALL
+    return "bulbosaur"
 
+def getRandomPokemonID():
+    return random.randint(0, MAX_POKEMON - 1) 
+
+def getSilhouetteImage(pokemonID):
+    return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+
+def getDecoyNames():
+    return ["pikachu", "crosshairs", "gregosaur"]
+
+@app.route('/pokemon', methods=['GET'])
 def getRandomPokemon():
-    pokemonID = getPokemonID()
-    silhouetteImage = getSilhouetteImage()
+    pokemonID = getRandomPokemonID()
+    silhouetteImage = getSilhouetteImage(pokemonID)
     decoyNames = getDecoyNames()
-    pokemonNamesList = trueName + decoyNames
+    trueName = getTrueName(pokemonID)
+    pokemonNamesList = [trueName] + decoyNames 
 
-    return jsonify({"pokemonID": pokemonID
-                    "silhouetteImage": silhouetteImage
-                    "decoyNames": decoyNames
-                    "pokemonNamesList" = pokemonNamesList
-                    })
+    return {"pokemonID": pokemonID,
+            "silhouetteImage": silhouetteImage,
+            "pokemonNamesList": pokemonNamesList
+    }
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
