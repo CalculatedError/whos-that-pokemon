cf052782fd00b54571ab1c1be6847c38e66c125b
Janek <jn.smialek@gmail.com>
2024-10-21 14:33:17 +0100

    first commit

---
+++ pokemon_back_end_api.py
@@ -0,0 +1,10 @@
+import requests
+
+#
+# The API should respond with the following information:
+# - The Pokémon ID.
+# - The silhouette image of the Pokémon.
+# - A list of four Pokémon names (the correct name and three decoy names)
+#
+
+# https://pokeapi.co/
