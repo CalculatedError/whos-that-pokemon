a3220932d811d690e05c88ac29d9221b9548dec8
Janek <jn.smialek@gmail.com>
2024-10-21 15:03:16 +0100

    followed a tutorial about pokeapi

--- pokemon_back_end_api.py
+++ pokemon_back_end_api.py
@@ -8,3 +8,25 @@ import requests
 #
 
 # https://pokeapi.co/
+
+base_url = "https://pokeapi.co/api/v2"
+
+def getPokemonInfo(name):
+    url = "{}/pokemon/{}".format(base_url, name)
+    response = requests.get(url)
+    print(response)
+
+    if response.status_code == 200:
+        print("Success: data retrieved")
+    else:
+        print("Failed to get data, error code: {}".format(response.status_code))
+
+    return response.json()
+
+pokemonName = "pikachu"
+pokemonInfo = getPokemonInfo(pokemonName)
+
+if pokemonInfo:
+    print(pokemonInfo["name"])
+    print(pokemonInfo["id"])
+    print(pokemonInfo["sprites"]["front_default"])
