97d58f39f224fa01a60c755285617149851fb619
Janek <jn.smialek@gmail.com>
2024-10-22 16:14:42 +0100

    small visual changes

--- pokemon_back_end_api.py
+++ pokemon_back_end_api.py
@@ -1,8 +1,15 @@
 import requests
 import flask
 from flask import jsonify
+from flask import Flask
+
+from flask import Flask, jsonify, send_from_directory
+import random
 
 MAX_POKEMON = 50
+baseUrl = "https://pokeapi.co/api/v2/"
+
+app = Flask(__name__)
 
 #
 # The API should respond with the following information:
@@ -10,15 +17,28 @@ MAX_POKEMON = 50
 # - The silhouette image of the Pokémon.
 # - A list of four Pokémon names (the correct name and three decoy names)
 #
+
+def getPokemonInfo(id):
+    url = "{}/pokemon/{}".format(baseUrl, id)
+    response = requests.get(url)
+    print(response)
+
+    if response.status_code == 200:
+        print("Success: data retrieved")
+    else:
+        print("Failed to get data, error code: {}".format(response.status_code))
+
+    return response.json()
+
 def getTrueName(pokemonID):
     # TODO REPLACE WITH API CALL
     return "bulbosaur"
 
 def getRandomPokemonID():
-    return random.randint(0, MAX_POKEMON - 1) 
+    return random.randint(1, MAX_POKEMON) 
 
-def getSilhouetteImage(pokemonID):
-    return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+def getSilhouetteImage(pokemonInfo, pokemonID):
+    return pokemonInfo["sprites"]["front_default"]
 
 def getDecoyNames():
     return ["pikachu", "crosshairs", "gregosaur"]
@@ -26,7 +46,8 @@ def getDecoyNames():
 @app.route('/pokemon', methods=['GET'])
 def getRandomPokemon():
     pokemonID = getRandomPokemonID()
-    silhouetteImage = getSilhouetteImage(pokemonID)
+    pokemonInfo = getPokemonInfo(pokemonID) 
+    silhouetteImage = getSilhouetteImage(pokemonInfo, pokemonID)
     decoyNames = getDecoyNames()
     trueName = getTrueName(pokemonID)
     pokemonNamesList = [trueName] + decoyNames 
@@ -38,7 +59,7 @@ def getRandomPokemon():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return send_from_directory('.', 'index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
